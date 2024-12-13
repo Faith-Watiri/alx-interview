63bdeada5c06e1247913efa0a71cc99dda9f8d6f
Faith-Watiri <watiri216@gmail.com>
2024-12-13 09:15:53 +0000

    commit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
+"""0. Prime Game - Maria and Ben are playing a game"""
 
-  def isWinner(x, nums):
-    """
-    Determines the winner of each game and returns the player
-    with the most wins.
 
-    :param x: Number of rounds (integer)
-    :param nums: List of integers representing n for each round
-    :return: Name of the player with the most wins or None if it's a tie
+def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
     """
-    if not nums or x < 1:
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
         return None
 
-    # Precompute prime numbers up to the maximum number in nums
-    max_num = max(nums)
-    is_prime = [True] * (max_num + 1)
-    is_prime[0] = is_prime[1] = False
+    ben = 0
+    maria = 0
 
-    for i in range(2, int(max_num**0.5) + 1):
-        if is_prime[i]:
-            for multiple in range(i * i, max_num + 1, i):
-                is_prime[multiple] = False
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
 
-    # Precompute the cumulative count of primes up to each number
-    prime_count = [0] * (max_num + 1)
-    for i in range(1, max_num + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
-
-    # Determine the winner for each round
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if prime_count[n] % 2 == 1:
-            maria_wins += 1
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+            maria += 1
+    if ben > maria:
         return "Ben"
-    else:
-        return None
+    if maria > ben:
+        return "Maria"
+    return None
 
-# Example usage
-  if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
